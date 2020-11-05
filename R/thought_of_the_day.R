203c6aad32c81349974853820fd84f3689ecf52e
scallahan4 <scottcallaha@umass.edu>
2020-11-05 14:02:40 -0500

    add Thought of day

---
+++ R/thought_of_the_day.R
@@ -0,0 +1,4 @@
+
+  thought<-function() {
+print("That bug lives in a world all his own...He doesn't know anything about
+atmoshpheric testing, strikes, farm problems, medical care, education,or income tax.-Snoopy") }
